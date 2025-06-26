39def7414a6a2f9469436e511b25805c393594b6
Michael Harhay <Michael_Harhay@arxtron.com>
2025-06-26 17:29:54 -0400

    Fix for real5

--- build_prj.bat
+++ build_prj.bat
@@ -8,7 +8,7 @@ set PROJECT_PATH=C:\Users\Michael_Harhay\Desktop\CICD_Test\CICD_TestProject.prj
 set CVI_EXE=compile.exe
 
 rem Run CVI to build the project
-%CVI_EXE% %PROJECT_PATH%
+%CVI_EXE% %PROJECT_PATH% -debug
 
 rem Check for errors
 if %errorlevel% neq 0 (
