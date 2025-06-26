cfa4fd6268b48e9648aaa95a0e51f743d83a8cfc
Michael Harhay <Michael_Harhay@arxtron.com>
2025-06-26 16:54:16 -0400

    TELJEWET

--- ProjectCode.c
+++ ProjectCode.c
@@ -4,3 +4,21 @@ int TestStep_SetX (int x) {
 	x = 1;
 	return x;
 }
+
+//==============================================================================
+// DLL main entry-point functions
+
+int __stdcall DllMain (HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+	switch (fdwReason) {
+		case DLL_PROCESS_ATTACH:
+			if (InitCVIRTE (hinstDLL, 0, 0) == 0)
+				return 0;	  /* out of memory */
+			break;
+		case DLL_PROCESS_DETACH:
+			CloseCVIRTE ();
+			break;
+	}
+	
+	return 1;
+}
