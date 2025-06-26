0518ed0a9e168cd675e541500f05e4c943036f46
Michael Harhay <Michael_Harhay@arxtron.com>
2025-06-26 11:46:46 -0400

    Test128u1092

--- build.bat
+++ build.bat
@@ -2,13 +2,13 @@
 echo [INFO] Starting CVI 2019 build...
 
 rem Set your project path
-set PROJECT_PATH=%cd%\CICD_TestProject.prj
+set PROJECT_PATH=C:\Users\Michael_Harhay\Desktop\CICD_Test\CICD_TestProject.prj
 
 rem Set CVI executable path (adjust if installed elsewhere)
-set CVI_EXE="C:\Program Files (x86)\National Instruments\CVI2019\compile.exe"
+set CVI_EXE=compile.exe
 
 rem Run CVI to build the project
-%CVI_EXE% %PROJECT_PATH% -build
+%CVI_EXE% %PROJECT_PATH%
 
 rem Check errorlevel and exit appropriately
 if %errorlevel% neq 0 (
